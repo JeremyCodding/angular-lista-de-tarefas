29a9cd1cc193af0736e7a476a1adafa8663c6d1f
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-05-09 09:29:46 -0300

    feat: Project finished

--- src/app/modules/lista-de-tarefas/pages/list/list.component.ts
+++ src/app/modules/lista-de-tarefas/pages/list/list.component.ts
@@ -2,6 +2,8 @@ import { Component, signal } from '@angular/core';
 import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
 import { IListItems } from '../../interface/IListItems.interface';
 import { InputListItemComponent } from '../../components/input-list-item/input-list-item.component';
+import { ELocalStorage } from '../../enums/ELocalStorage.enum';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-list',
@@ -17,8 +19,16 @@ export class ListComponent {
   public getListItems = this.#setListItems.asReadonly();
 
   #parseItems() {
-    return JSON.parse(localStorage.getItem('@my-list') || '[]');
+    return JSON.parse(localStorage.getItem(ELocalStorage.MY_LIST) || '[]');
   }
+
+  #updateLocalStorage() {
+    return localStorage.setItem(
+      ELocalStorage.MY_LIST,
+      JSON.stringify(this.#setListItems())
+    );
+  }
+
   public getInputAndAddItem(value: IListItems) {
     localStorage.setItem(
       '@my-list',
@@ -27,8 +37,83 @@ export class ListComponent {
 
     return this.#setListItems.set(this.#parseItems());
   }
+
+  public listItemsStage(value: 'pending' | 'completed') {
+    return this.getListItems().filter((item: IListItems) =>
+      value == 'pending' ? !item.checked : item.checked
+    );
+  }
+
+  public updateItemCheckbox(newItem: { id: string; checked: boolean }) {
+    this.#setListItems.update((oldValue: IListItems[]) => {
+      oldValue.filter((res) => {
+        if (res.id === newItem.id) {
+          res.checked = newItem.checked;
+          return res;
+        }
+
+        return res;
+      });
+
+      return oldValue;
+    });
+
+    return this.#updateLocalStorage();
+  }
+
+  public updateItemText(newItem: { id: string; value: string }) {
+    this.#setListItems.update((oldValue: IListItems[]) => {
+      oldValue.filter((res) => {
+        if (res.id === newItem.id) {
+          res.value = newItem.value;
+          return res;
+        }
+
+        return res;
+      });
+
+      return oldValue;
+    });
+
+    return this.#updateLocalStorage();
+  }
+
+  public outputDeleteItem(id: string) {
+    Swal.fire({
+      title: 'Tem certeza de quer deletar todas as tarefas?',
+      text: 'Isso é irreversível!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sim, delete!',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.#setListItems.update((oldValue: IListItems[]) =>
+          oldValue.filter((res: IListItems) => res.id !== id)
+        );
+
+        return this.#updateLocalStorage();
+      }
+    });
+  }
+
   public deleteAllItems() {
-    localStorage.removeItem('@my-list');
-    return this.#setListItems.set(this.#parseItems());
+    Swal.fire({
+      title: 'Tem certeza de quer deletar todas as tarefas?',
+      text: 'Isso é irreversível!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sim, delete!',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem('@my-list');
+        return this.#setListItems.set(this.#parseItems());
+      }
+    });
   }
 }
